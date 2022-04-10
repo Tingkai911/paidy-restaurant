e1987422c49f1c21176fc6f8bddbbc1fbae48b01
Ting Kai <tingkai911@gmail.com>
2022-04-10 09:32:42 +0800

    add unit test cases

--- src/test/java/com/paidy/restaurant/service/MenuServiceTest.java
+++ src/test/java/com/paidy/restaurant/service/MenuServiceTest.java
@@ -1,8 +1,16 @@
 package com.paidy.restaurant.service;
 
-import static org.mockito.Mockito.mock;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
 
+import com.paidy.restaurant.exception.MenuServiceException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mybatis.spring.SqlSessionTemplate;
 
 public class MenuServiceTest {
@@ -14,5 +22,48 @@ public class MenuServiceTest {
   public void init() {
     sqlSessionTemplate = mock(SqlSessionTemplate.class);
     menuService = new MenuService(sqlSessionTemplate, pageLimit);
+  }
+
+  @Test
+  public void testGetAllMenuItemsSuccess() {
+    ArgumentCaptor<String> stringArgumentCaptor = ArgumentCaptor.forClass(String.class);
+    ArgumentCaptor<Map> mapArgumentCaptor = ArgumentCaptor.forClass(Map.class);
+    when(sqlSessionTemplate.selectList(stringArgumentCaptor.capture(), mapArgumentCaptor.capture()))
+        .thenReturn(new ArrayList<>());
+    menuService.getAllMenuItems(1);
+    assertEquals("getAllMenuItems", stringArgumentCaptor.getValue());
+    Map<String, Object> expected = new HashMap<>();
+    expected.put("limit", pageLimit);
+    expected.put("offset", 0);
+    Map<String, Object> actual = mapArgumentCaptor.getValue();
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testGetAllMenuItemsFail() {
+    when(sqlSessionTemplate.selectList(anyString(), any())).thenThrow(MenuServiceException.class);
+    assertThrows(MenuServiceException.class, () -> menuService.getAllMenuItems(1));
+  }
+
+  @Test
+  public void testSearchMenuItemsByNameSuccess() {
+    ArgumentCaptor<String> stringArgumentCaptor = ArgumentCaptor.forClass(String.class);
+    ArgumentCaptor<Map> mapArgumentCaptor = ArgumentCaptor.forClass(Map.class);
+    when(sqlSessionTemplate.selectList(stringArgumentCaptor.capture(), mapArgumentCaptor.capture()))
+        .thenReturn(new ArrayList<>());
+    menuService.searchMenuItemsByName(1, "ramen");
+    assertEquals("searchMenuItemsByName", stringArgumentCaptor.getValue());
+    Map<String, Object> expected = new HashMap<>();
+    expected.put("limit", pageLimit);
+    expected.put("offset", 0);
+    expected.put("name", "%ramen%");
+    Map<String, Object> actual = mapArgumentCaptor.getValue();
+    assertEquals(expected, actual);
+  }
+
+  @Test
+  public void testSearchMenuItemsByNameFail() {
+    when(sqlSessionTemplate.selectList(anyString(), any())).thenThrow(MenuServiceException.class);
+    assertThrows(MenuServiceException.class, () -> menuService.searchMenuItemsByName(1, "ramen"));
   }
 }
--- src/test/java/com/paidy/restaurant/service/OrderServiceTest.java
+++ src/test/java/com/paidy/restaurant/service/OrderServiceTest.java
@@ -1,8 +1,18 @@
 package com.paidy.restaurant.service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import com.paidy.restaurant.db.entity.Order;
+import com.paidy.restaurant.exception.OrderServiceException;
+import java.util.*;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mybatis.spring.SqlSessionTemplate;
 
 public class OrderServiceTest {
@@ -14,5 +24,144 @@ public class OrderServiceTest {
   public void init() {
     sqlSessionTemplate = mock(SqlSessionTemplate.class);
     orderService = new OrderService(sqlSessionTemplate, pageLimit);
+  }
+
+  @Test
+  public void testUpdateOrderSuccess() {
+    Order order = new Order();
+    order.setStaffId(100L);
+    ArgumentCaptor<String> stringArgumentCaptor = ArgumentCaptor.forClass(String.class);
+    ArgumentCaptor<Order> orderArgumentCaptor = ArgumentCaptor.forClass(Order.class);
+    when(sqlSessionTemplate.update(stringArgumentCaptor.capture(), orderArgumentCaptor.capture()))
+        .thenReturn(1);
+    orderService.updateOrder(order);
+    assertEquals("updateOrder", stringArgumentCaptor.getValue());
+    assertEquals(order, orderArgumentCaptor.getValue());
+  }
+
+  @Test
+  public void testUpdateOrderFail() {
+    Order order = new Order();
+    order.setStaffId(100L);
+    when(sqlSessionTemplate.update(anyString(), any())).thenThrow(OrderServiceException.class);
+    assertThrows(OrderServiceException.class, () -> orderService.updateOrder(order));
+  }
+
+  @Test
+  public void testDeleteOrderSuccess() {
+    when(sqlSessionTemplate.update("deleteOrder", 1)).thenReturn(1);
+    orderService.deleteOrder(1);
+  }
+
+  @Test
+  public void testDeleteOrderFail() {
+    when(sqlSessionTemplate.delete(anyString(), any())).thenThrow(OrderServiceException.class);
+    assertThrows(OrderServiceException.class, () -> orderService.deleteOrder(1));
+  }
+
+  @Test
+  public void testInsertOrderSuccess() {
+    Order order = new Order();
+    order.setStaffId(100L);
+    List<Order> orderList = Arrays.asList(order);
+    ArgumentCaptor<String> stringArgumentCaptor = ArgumentCaptor.forClass(String.class);
+    ArgumentCaptor<Map> mapArgumentCaptor = ArgumentCaptor.forClass(Map.class);
+    when(sqlSessionTemplate.insert(stringArgumentCaptor.capture(), mapArgumentCaptor.capture()))
+        .thenReturn(1);
+    orderService.insertOrders(orderList);
+    assertEquals("insertOrders", stringArgumentCaptor.getValue());
+    Map<String, Object> params = new HashMap<>();
+    params.put("orderList", orderList);
+    assertEquals(params, mapArgumentCaptor.getValue());
+  }
+
+  @Test
+  public void testInsertOrderFailed() {
+    Order order = new Order();
+    order.setStaffId(100L);
+    List<Order> orderList = Arrays.asList(order);
+    when(sqlSessionTemplate.insert(anyString(), any())).thenThrow(OrderServiceException.class);
+    assertThrows(OrderServiceException.class, () -> orderService.insertOrders(orderList));
+  }
+
+  @Test
+  public void testGetOrdersByTableNumberSuccess1() {
+    ArgumentCaptor<String> stringArgumentCaptor = ArgumentCaptor.forClass(String.class);
+    ArgumentCaptor<Map> mapArgumentCaptor = ArgumentCaptor.forClass(Map.class);
+    when(sqlSessionTemplate.selectList(stringArgumentCaptor.capture(), mapArgumentCaptor.capture()))
+        .thenReturn(new ArrayList<>());
+    orderService.getOrdersByTableNumber(1, 1, true);
+    assertEquals("selectServedOrdersByTableNumber", stringArgumentCaptor.getValue());
+    Map<String, Object> param = new HashMap<>();
+    param.put("limit", pageLimit);
+    param.put("offset", 0);
+    param.put("tableNumber", 1);
+    param.put("status", "%served%");
+    assertEquals(param, mapArgumentCaptor.getValue());
+  }
+
+  @Test
+  public void testGetOrdersByTableNumberSuccess2() {
+    ArgumentCaptor<String> stringArgumentCaptor = ArgumentCaptor.forClass(String.class);
+    ArgumentCaptor<Map> mapArgumentCaptor = ArgumentCaptor.forClass(Map.class);
+    when(sqlSessionTemplate.selectList(stringArgumentCaptor.capture(), mapArgumentCaptor.capture()))
+        .thenReturn(new ArrayList<>());
+    orderService.getOrdersByTableNumber(1, 1, false);
+    assertEquals("selectNotServedOrdersByTableNumber", stringArgumentCaptor.getValue());
+    Map<String, Object> param = new HashMap<>();
+    param.put("limit", pageLimit);
+    param.put("offset", 0);
+    param.put("tableNumber", 1);
+    param.put("status", "%served%");
+    assertEquals(param, mapArgumentCaptor.getValue());
+  }
+
+  @Test
+  public void testGetOrdersByTableNumberFail() {
+    when(sqlSessionTemplate.selectList(anyString(), any())).thenThrow(OrderServiceException.class);
+    assertThrows(
+        OrderServiceException.class, () -> orderService.getOrdersByTableNumber(1, 1, true));
+  }
+
+  @Test
+  public void testGetOrdersByTableNumberAndMenuIdSuccess1() {
+    ArgumentCaptor<String> stringArgumentCaptor = ArgumentCaptor.forClass(String.class);
+    ArgumentCaptor<Map> mapArgumentCaptor = ArgumentCaptor.forClass(Map.class);
+    when(sqlSessionTemplate.selectList(stringArgumentCaptor.capture(), mapArgumentCaptor.capture()))
+        .thenReturn(new ArrayList<>());
+    orderService.getOrdersByTableNumberAndMenuId(1, 1, true, 2);
+    assertEquals("selectServedOrdersByMenuIdAndTableNumber", stringArgumentCaptor.getValue());
+    Map<String, Object> param = new HashMap<>();
+    param.put("limit", pageLimit);
+    param.put("offset", 0);
+    param.put("menuId", 2);
+    param.put("tableNumber", 1);
+    param.put("status", "%served%");
+    assertEquals(param, mapArgumentCaptor.getValue());
+  }
+
+  @Test
+  public void testGetOrdersByTableNumberAndMenuIdSuccess2() {
+    ArgumentCaptor<String> stringArgumentCaptor = ArgumentCaptor.forClass(String.class);
+    ArgumentCaptor<Map> mapArgumentCaptor = ArgumentCaptor.forClass(Map.class);
+    when(sqlSessionTemplate.selectList(stringArgumentCaptor.capture(), mapArgumentCaptor.capture()))
+        .thenReturn(new ArrayList<>());
+    orderService.getOrdersByTableNumberAndMenuId(1, 1, false, 2);
+    assertEquals("selectNotServedOrdersByMenuIdAndTableNumber", stringArgumentCaptor.getValue());
+    Map<String, Object> param = new HashMap<>();
+    param.put("limit", pageLimit);
+    param.put("offset", 0);
+    param.put("menuId", 2);
+    param.put("tableNumber", 1);
+    param.put("status", "%served%");
+    assertEquals(param, mapArgumentCaptor.getValue());
+  }
+
+  @Test
+  public void testGetOrdersByTableNumberAndMenuIdFail() {
+    when(sqlSessionTemplate.selectList(anyString(), any())).thenThrow(OrderServiceException.class);
+    assertThrows(
+        OrderServiceException.class,
+        () -> orderService.getOrdersByTableNumberAndMenuId(1, 1, true, 2));
   }
 }
